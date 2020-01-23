e6ba5a873adeecf8e0493f5808e4e5ff1cf4460b
Simon Coffey <simon@urbanautomaton.com>
2020-01-23 22:10:49 +0000

    Variable angle according to Truchet parameter
    This is a single gradient that appears to rotate about the div's centre
    point with the desired angle [22.5deg, 67.5deg] according to the input
    parameter.
    
    I guess to pin it in the corner I'll need to vary the percentage along
    the gradient at which the transition occurs. Next commit!

--- truchet/src/index.jsx
+++ truchet/src/index.jsx
@@ -1,8 +1,20 @@
 'use strict';
 
 const Truchet = (t) => (
-  <span className="truchet" style={{ "--t": `${t * 100}%` }}  />
+  <div className="truchet" style={{ "--t": t }}  />
 );
 
 const domContainer = document.querySelector('#container');
-ReactDOM.render([Truchet(0.3), Truchet(0.7)], domContainer);
+ReactDOM.render([
+  Truchet(0.0),
+  Truchet(0.1),
+  Truchet(0.2),
+  Truchet(0.3),
+  Truchet(0.4),
+  Truchet(0.5),
+  Truchet(0.6),
+  Truchet(0.7),
+  Truchet(0.8),
+  Truchet(0.9),
+  Truchet(1.0),
+], domContainer);
